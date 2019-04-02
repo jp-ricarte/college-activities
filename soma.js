ce5b20234b5d060a1470c69951f1896cebd37832
João Pedro Ricarte <49175128+jp-ricarte@users.noreply.github.com>
2019-04-02 01:04:50 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,10 @@
+n = prompt("quantos numeros")
+i = 0
+soma = parseInt(numero)
+soma = 0
+while(i < n){
+	var numero = prompt("valor do numero")
+	i = i + 1
+	soma = (parseInt(numero) + soma)
+}
+alert(soma)
