af15fd08ef7a8c54a40a79dfd67dd6229e941724
João Pedro Ricarte <49175128+jp-ricarte@users.noreply.github.com>
2019-04-03 00:14:30 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,6 @@
+
+var numero = prompt("digite o numero que deseja saber a tabuada");
+
+for(multiplicador = 1; multiplicador <= 10; multiplicador++){
+	document.write(numero + " x " + multiplicador + " = " + numero * multiplicador + "<br>")
+}
